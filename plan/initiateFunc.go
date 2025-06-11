d9da691974ba5a94aa447ffc1a0a2019232243b7
chung-U <qscesz0411@gmail.com>
2025-06-11 22:11:07 +0000

    osro 0612-2

--- plan/initiateFunc.go
+++ plan/initiateFunc.go
@@ -96,9 +96,9 @@ func (plan *OSRO) Initiate_Plan() {
 		// plan.SP.Objs_SP=append(plan.SP.Objs_SP, result)//要改SP 變成4個
 		// fmt.Printf("method=%s obj=%v\n", m, Objs_sp)
 	}
-	file_sp := "sp_history0612.xlsx"
+	file_sp := "sp_history0612--important_can 100 --unimportant_can 500 --input_tsn 30 --input_avb 70 --bg_tsn 30 --bg_avb 18.xlsx"
 	var all_sp []*algo.Result
-	
+
 	for _, sp := range plan.SP.Objs_SP {
 		all_sp = append(all_sp, sp) // 每種方法可能 append 多筆 epoch 結果
 	}
@@ -134,7 +134,7 @@ func (plan *OSRO) Initiate_Plan() {
 		// plan.SP.Objs_SP=append(plan.SP.Objs_SP, result)//要改SP 變成4個
 		// fmt.Printf("method=%s obj=%v\n", m, Objs_sp)
 	}
-	file_osro := "osro_history0612.xlsx"
+	file_osro := "osro_history0612--important_can 100 --unimportant_can 500 --input_tsn 30 --input_avb 70 --bg_tsn 30 --bg_avb 18.xlsx"
 	var all []*algo.Result
 	for _, osro := range plan.OSRO_method {
 		all = append(all, osro.Objs_osro...) // 每種方法可能 append 多筆 epoch 結果
